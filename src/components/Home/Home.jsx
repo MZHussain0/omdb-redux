77f7dfdd2518829d6e592f4e9504bd2b2f5ee522
Zakir Hussain <mzhussain092@gmail.com>
2022-12-06 16:42:24 +0530

    implemented redux-asynThunk

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,13 @@
-import React, { Fragment } from "react";
-import { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import MovieApi from "../../common/Apis/MovieApi";
-import { APIKey } from "../../common/Apis/MovieApiKey";
-import { addMovies } from "../../features/movies/movieSlice";
+import { fetchAsyncMovies } from "../../features/movies/movieSlice";
 import MovieListing from "../MovieListing/MovieListing";
 
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    const movieText = "Galaxy";
-    const fetchMovies = async () => {
-      const response = await MovieApi.get(
-        `?apikey=${APIKey}&s=${movieText}&type=movie`
-      ).catch((err) => {
-        console.log("Err:", err);
-      });
-      dispatch(addMovies(response.data));
-    };
-    fetchMovies();
-  }, []);
-
+    dispatch(fetchAsyncMovies());
+  }, [dispatch]);
   return (
     <Fragment>
       <div className="banner-image"></div>
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import MovieApi from "../../common/Apis/MovieApi";
+import { APIKey } from "../../common/Apis/MovieApiKey";
+
+export const fetchAsyncMovies = createAsyncThunk(
+  "movies/fetchAsyncMovies",
+  async () => {
+    const movieText = "Galaxy";
+    const response = await MovieApi.get(
+      `?apikey=${APIKey}&s=${movieText}&type=movie`
+    );
+    return response.data;
+  }
+);
 
 const initialState = {
   movies: {},
@@ -11,6 +24,20 @@ const movieSlice = createSlice({
     addMovies: (state, { payload }) => {
       state.movies = payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAsyncMovies.pending, (state, action) => {
+        console.log("Pending");
+      })
+      .addCase(fetchAsyncMovies.fulfilled, (state, { payload }) => {
+        console.log("Fetched successfully");
+        return { ...state, movies: payload };
+      })
+      .addCase(fetchAsyncMovies.rejected, (state, action) => {
+        state.status = "Rejected";
+        console.log("Rejected");
+      });
   },
 });
 
